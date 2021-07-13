8e7cd30fca691391dceb7b5cd364a6364b58f829
kimseungmun <utmdans@likelion.org>
2021-07-13 10:06:23 +0900

    7/13 ODIN 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,12 +2,22 @@ from django.http import request, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import NewModel
 
 
 def hello_world(request):
     if request.method == "POST":
+
+        temp = request.POST.get('input_text')
+
+        model_instance = NewModel()
+        model_instance.text = temp
+        model_instance.save()
+
+        data_list = NewModel.objects.all()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'POST METHOD!'})
+                      context={'data_list': data_list})
     else:
         return render(request, 'accountapp/hello_world.html',
                       context={'text': 'GET METHOD!'})
