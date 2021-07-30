ced719595a493ba82a4bac5869499f5ae7da819a
kimseungmun <utmdans@likelion.org>
2021-07-30 09:20:24 +0900

    7/30 Týr 1

--- profileapp/views.py
+++ profileapp/views.py
@@ -15,12 +15,14 @@ from profileapp.models import Profile
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk' : self.object.user.pk})
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -28,6 +30,7 @@ class ProfileUpdateView(UpdateView): # 논리
     model = Profile
     form_class = ProfileCreationForm
     context_object_name = 'target_profile'
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html' # <= 을 사용해서 만들어주겠다.
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk' : self.object.user.pk})
