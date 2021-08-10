cbf02e8e974b793e200d023f346501a83091da67
kimseungmun <utmdans@likelion.org>
2021-08-10 10:43:57 +0900

    8/10 Sigmundr 5

--- commentapp/views.py
+++ commentapp/views.py
@@ -13,5 +13,12 @@ class CommentCreateView(CreateView):
     form_class = CommentCreationForm
     template_name = 'commentapp/create.html'
 
+    def form_valid(self, form):
+        form.instance.writer = self.request.user # 작성자 설정
+        # form.instance.article = Article.objects.get(pk=self.request.POST.get('article_pk'))
+        # 특정한 게시글을 우리가 작성할 게시글에 연결
+        form.instance.article_id = self.request.POST.get('article_pk')
+        return super().form_valid(form)
+
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk' : self.object.article.pk})
