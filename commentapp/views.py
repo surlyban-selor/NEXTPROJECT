62984cc10a8fc78d9ff2f7747fa66df2085ad448
kimseungmun <utmdans@likelion.org>
2021-08-11 14:47:41 +0900

    8/11 Bifröst 1

--- commentapp/views.py
+++ commentapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
@@ -22,3 +22,13 @@ class CommentCreateView(CreateView):
 
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk' : self.object.article.pk})
+
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+        # 받은 값을 그대로 준다. 원래 연결되어 있었던 게시물을 detail로
